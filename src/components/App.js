81824c27926b6974cf7b44490030d7e585473581
Emmakaranja1 <karanjaemmak@gmail.com>
2025-04-21 02:48:24 +0300

    fix: correct import path for Header component and add Filter component for category selection

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
+import Header from "./Header";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
